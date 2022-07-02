1c8a331cd8eb8262a406936d282395dffba097f2
Allen Mathews <allenjoemathews@gmail.com>
2022-07-02 18:36:26 +1000

    render multiple diece

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const hero = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: 6
+    diceRoll: 3,
+    diceCount: 3
 }
 
 const monster = {
@@ -11,17 +12,28 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: 4
+    diceRoll: 4,
+    diceCount: 1
 }
 
 function renderCharacter(data) {
-    document.getElementById(data.elementId).innerHTML =
+    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+
+
+
+    let diceHtml = ''
+
+    for (let i = 0; i < diceCount; i++) {
+        diceHtml += `<div class="dice">6</div>`
+    }
+
+    document.getElementById(elementId).innerHTML =
         `<div class="character-card">
-            <h4 class="name"> ${data.name} </h4>
-            <img class="avatar" src="${data.avatar}" />
-            <div class="health">health: <b> ${data.health} </b></div>
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
             <div class="dice-container">
-                <div class="dice"> ${data.diceRoll} </div>
+                ${diceHtml}
             </div>
         </div>`
 }
