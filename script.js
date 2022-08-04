9e01aa5e39f274bc2d3106c752640283677c4bb0
Allen Mathews <allenjoemathews@gmail.com>
2022-08-04 20:48:43 +1000

    changed script

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
 function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function() {
-        return Math.floor(Math.random() * 6) + 1
-    })
-
-
-    //dice court
-    function getDiceHtml(diceCount) {
-        return getDiceRollArray(diceCount).map(function(num) {
-            return `<div class="dice">${num}</div>`
-        }).join('')
+    let newDiceRolls = [];
+    for (let i = 0; i < diceCount; i++) {
+        newDiceRolls.push(Math.floor(Math.random() * 6) + 1);
     }
+    return newDiceRolls;
+}
 
-    const hero = {
-        elementId: "hero",
-        name: "Wizard",
-        avatar: "images/wizard.png",
-        health: 60,
-        diceCount: 3
-    }
+function getDiceHtml(diceCount) {
+    return getDiceRollArray(diceCount).map(function(num) {
+        return `<div class="dice">${num}</div>`
+    }).join('')
+}
 
-    const monster = {
-        elementId: "monster",
-        name: "Orc",
-        avatar: "images/orc.png",
-        health: 10,
-        diceCount: 1
-    }
 
-    function renderCharacter(data) {
-        const { elementId, name, avatar, health, diceCount } = data;
-        const diceHtml = getDiceHtml(diceCount)
+const hero = {
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
 
-        document.getElementById(elementId).innerHTML =
-            `<div class="character-card">
+    diceCount: 3
+}
+
+const monster = {
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: [6],
+    diceCount: 1
+}
+
+function renderCharacter(data) {
+    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount)
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
@@ -40,7 +44,7 @@ function getDiceRollArray(diceCount) {
                 ${diceHtml}
             </div>
         </div>`;
-    }
+}
 
-    renderCharacter(hero);
-    renderCharacter(monster);
+renderCharacter(hero);
+renderCharacter(monster);
