b46d4cee9fd02f3a945127ed263baafa68c90efa
Allen Mathews <allenjoemathews@gmail.com>
2022-08-15 19:00:27 +1000

    changed contructor

--- script.js
+++ script.js
@@ -26,23 +26,21 @@ const monster = {
     diceCount: 1
 }
 
+
+
 function Character(data) {
-    this.elementId = data.elementId
+    this.elementId = data.elementId;
     this.name = data.name;
     this.avatar = data.avatar;
     this.health = data.health;
     this.diceCount = data.diceCount;
-}
 
+    this.getCharacterHtml = function() {
+        const { elementId, name, avatar, health, diceCount } = this;
+        const diceHtml = getDiceHtml(diceCount)
 
-
-
-function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceCount } = data;
-    const diceHtml = getDiceHtml(diceCount)
-
-    document.getElementById(elementId).innerHTML =
-        `<div class="character-card">
+        document.getElementById(elementId).innerHTML =
+            `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
@@ -50,7 +48,12 @@ function renderCharacter(data) {
                 ${diceHtml}
             </div>
         </div>`;
+
+    }
+
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+const wizard = new Character(hero)
+const orc = new Character(monster)
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
