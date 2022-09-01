4ff6192392cac840d267cfe904f34c65fc3abbf6
Allen Mathews <allenjoemathews@gmail.com>
2022-09-02 08:09:26 +1000

    changed the this statement

--- js/character.js
+++ js/character.js
@@ -1,7 +1,7 @@
-import { getDiceRollArray, getDicePlaceholderHtml } from './utils'
-
 function Character(data) {
     Object.assign(this, data)
+
+    this.diceArray = getDicePlaceholderHtml(this.diceCount)
 
     this.getDiceHtml = function(diceCount) {
         return getDiceRollArray(diceCount).map(function(num) {
@@ -12,14 +12,13 @@ function Character(data) {
     this.getCharacterHtml = function() {
         const { elementId, name, avatar, health, diceCount } = this;
         let diceHtml = this.getDiceHtml(diceCount);
-
         return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
                 <div class="dice-container">
-                    ${diceHtml}
+                    ${this.diceArray}
                 </div>
             </div>`
     }
